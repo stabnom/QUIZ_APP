8afa6b028dcd9d6f82487e6664033918cd2532b9
Mikołaj Stabno <stabnom@gmail.com>
2025-08-31 11:00:17 +0200

    course continuing

--- quiz/lib/questionScreen.dart
+++ quiz/lib/questionScreen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:quiz/answer_button.dart';
+import 'package:quiz/data/questions.dart';
 
 class QuestionScreen extends StatefulWidget {
   const QuestionScreen({super.key});
@@ -13,16 +14,21 @@ class QuestionScreen extends StatefulWidget {
 class _QuestionScreen extends State<QuestionScreen> {
   @override
   Widget build(context) {
+    final currentQuestion = questions[0];
+
     return SizedBox(
       width: double.infinity,
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          const Text(style: TextStyle(color: Colors.white), "test"),
+          Text(
+            currentQuestion.question,
+            style: TextStyle(color: Colors.white)),
           SizedBox(height: 30),
-          AnswerButton(answerText: "Odp", onTap: () {}),
-          AnswerButton(answerText: "Odp", onTap: () {}),
-          AnswerButton(answerText: "Odp", onTap: () {}),
+          AnswerButton(answerText: currentQuestion.answers[0], onTap: () {}),
+          AnswerButton(answerText: currentQuestion.answers[1], onTap: () {}),
+          AnswerButton(answerText: currentQuestion.answers[2], onTap: () {}),
+          AnswerButton(answerText: currentQuestion.answers[3], onTap: () {}),
         ],
       ),
     );
